26ad84f376903b0521c9abf8ccfcb87d5d1064b2
Satyam Palkar <73163418+SatyamPalkar@users.noreply.github.com>
2025-04-04 20:49:43 +1100

    New files added

--- test_api.py
+++ test_api.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://localhost:8000/parse_resume/'
-file_path = r"C:\Users\satya\OneDrive\Desktop\Jobs\Data Analyst\Satyam_Palkar_Data_Analyst.pdf"
+file_path = r"C:\Users\satya\OneDrive\Desktop\Jobs\Data Analyst\Kirthi_resume.pdf"
 
 with open(file_path, 'rb') as file:
     response = requests.post(url, files={'file': file})
